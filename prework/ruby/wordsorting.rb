a4ecf1d6501e73dce1165744737172a2e9b3f620
alina <aliina.tudose@gmail.com>
2016-06-01 16:29:14 +0200

    wordsorting fixed

--- prework/ruby/wordsorting.rb
+++ prework/ruby/wordsorting.rb
@@ -1,13 +1,11 @@
+
 puts "Write here some text:"
 text = gets.chomp
-words = text.split
+words = text.scan(/[\w']+/)
 
-def alphabetize (arr)
+def alphabetize(arr)
     arr.sort {|item1, item2| item1.upcase <=> item2.upcase}
 end
 
-words.each do |word|
-    word = word.gsub(/\p{P}/, "")
-end
 
 puts "#{alphabetize(words)}"
