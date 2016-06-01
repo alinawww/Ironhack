bc2455737b36ead659436f886a3150956a92b262
alina <aliina.tudose@gmail.com>
2016-06-01 12:42:34 +0200

    fix punctuation and special characters

---
+++ prework/ruby/caesars-cypher/caesar3.rb
@@ -0,0 +1,21 @@
+def solve_cipher(input, shift=-3)
+    letters = input.split(//)
+    letters.each do |letter|
+        order = letter.ord
+        if letter == " "
+            new_order = order.to_i
+            elsif order < 64 && order > 32
+            new_order = order.to_i
+            else
+            new_order = order.to_i + shift
+        end
+        
+        if new_order < 97 && new_order > 90
+            print "z"
+            else
+            print new_order.chr
+        end
+    end
+end
+
+solve_cipher("p| uhdo qdph lv grqdog gxfn")
