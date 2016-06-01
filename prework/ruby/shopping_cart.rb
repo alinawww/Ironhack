e35f867aa91c18cead692a7aacf65a4442e3e261
alina <aliina.tudose@gmail.com>
2016-06-01 19:04:19 +0200

    shopping_cart iterations 1 to 3

---
+++ prework/ruby/shopping_cart.rb
@@ -0,0 +1,87 @@
+
+class Item
+    attr_reader :price_of_item, :name_of_item
+    def initialize(name_of_item, base_price)
+        @name_of_item = name_of_item
+        @base_price = base_price
+    end
+    def price_of_item
+        total_price_of_item = @base_price
+        return total_price_of_item
+    end
+end
+
+class Fruit < Item
+    def price_of_item
+        total_price_of_item = @base_price
+        if day_of_purchase = "Saturday" || day_of_purchase = "Sunday"
+            total_price_of_item = @base_price - 0.1 * @base_price
+        else
+            total_price_of_item = @base_price
+        end
+        return total_price_of_item
+    end
+end
+
+class Houseware < Item
+    def price_of_item
+        if @base_price > 100
+            total_price_of_item = @base_price - 0.05 * @base_price
+        else
+            total_price_of_item = @base_price
+        end
+        return total_price_of_item
+        
+    end
+end
+
+class ShoppingCart
+    def initialize
+        @items = []
+    end
+    def shopping_time(day_of_purchase)
+        @day_of_purchase = day_of_purchase
+    end
+    def add_to_cart(item)
+        @items.push(item)
+    end
+    def list_products
+        @items.each do |item|
+            total += item.price_of_item
+            puts item => item.price_of_item
+        end
+        return total
+    end
+    def checkout
+        total = 0
+        @items.each do |item|
+            total += item.price_of_item
+            puts "#{item.name_of_item} => $#{item.price_of_item}"
+        end
+        puts "-------------------------------------------"
+        return total
+    end
+end
+
+banana = Fruit.new("Bananas", 2)
+#orange_juice = Fruit.new("Orange Juice", 10)
+banana = Fruit.new("Banana", 10)
+vacuum_cleaner = Houseware.new("Vacuum Cleaner", 150)
+oj = Item.new("Orange Juice", 10)
+rice = Item.new("Rice", 1)
+anchovies = Item.new("Anchovies", 2)
+table = Houseware.new("Table", 50)
+
+
+alina = ShoppingCart.new
+alina.add_to_cart(vacuum_cleaner)
+alina.add_to_cart(table)
+alina.add_to_cart(rice)
+alina.add_to_cart(banana)
+alina.shopping_time("Saturday")
+
+puts "-------------------------------------------"
+puts "This is your order"
+puts "-------------------------------------------"
+puts "Your total is: $#{alina.checkout}. Have a nice day!"
+puts "-------------------------------------------"
