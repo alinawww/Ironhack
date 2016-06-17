864133a90aaf7e2f9876d7a3035edbbe112428e7
alina <aliina.tudose@gmail.com>
2016-06-17 11:57:31 +0200

    Shopping cart - iteration2

--- Class/week1/day4/shoppingcart.rb
+++ Class/week1/day4/shoppingcart.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Item
-  attr_reader :name, :price
+  attr_accessor :name, :price
   def initialize(name, price)
     @name = name
     @price = price
@@ -9,22 +9,61 @@ class Item
 end
 
 class ShoppingCart
+   attr_accessor :inventory
   def initialize
     @inventory = {}
   end
   def add_item_to_cart(item)
-    # @inventory.each do |item|
-      #binding.pry
-      if @inventory.key?(item.name.to_sym)
-        @inventory[item.name.to_sym][:quantity] += 1
-        @inventory[item.name.to_sym][:price] += @inventory[item.name.to_sym][:price]
+    #  binding.pry
+    if @inventory.key?(item.name.to_sym)
+      @inventory[item.name.to_sym][:price] += @inventory[item.name.to_sym][:price]/@inventory[item.name.to_sym][:quantity]
+      @inventory[item.name.to_sym][:quantity] += 1
 
+    else
+      @inventory[item.name.to_sym] = {:quantity => 1, :price => item.price}
+    end
+  end
+  def two_for_one(item)
+    #  binding.pry
+    if @inventory.key?(item.name.to_sym)
+      if @inventory[item.name.to_sym][:quantity] % 2 == 0
+        @inventory[item.name.to_sym][:price] = @inventory[item.name.to_sym][:price] / 2
+        # puts "#{@inventory[item.name.to_sym][:price]}*"
       else
-        @inventory[item.name.to_sym] = {:quantity => 1, :price => item.price}
-        # @inventory[item.name.to_sym][:quantity] = 1
-        # @inventory[item.name.to_sym][:price] = item.price
+        unit_price = @inventory[item.name.to_sym][:price] / @inventory[item.name.to_sym][:quantity]
+        @inventory[item.name.to_sym][:price] = (@inventory[item.name.to_sym][:price] + unit_price) / 2
+        return @inventory[item.name.to_sym][:price]
       end
-    # end
+    end
+  end
+  def three_for_two(item)
+    #  binding.pry
+    if @inventory.key?(item.name.to_sym)
+      if @inventory[item.name.to_sym][:quantity] % 3 == 0
+        @inventory[item.name.to_sym][:price] = (@inventory[item.name.to_sym][:price] / 3) * 2
+      else
+        remainder =  @inventory[item.name.to_sym][:quantity] % 3
+        unit_price = @inventory[item.name.to_sym][:price] / @inventory[item.name.to_sym][:quantity]
+        @inventory[item.name.to_sym][:price] = @inventory[item.name.to_sym][:price]/2 + unit_price * remainder
+        return @inventory[item.name.to_sym][:price]
+      end
+    end
+  end
+  def get_free_banana(item, freebie)
+    #  binding.pry
+    if @inventory.key?(item.name.to_sym)
+        if @inventory[item.name.to_sym][:quantity] > 4
+        groups_of_four =  (@inventory[item.name.to_sym][:quantity] / 4).floor
+          puts "**BONUS**\nFor every 4 #{item.name} you get a free banana. Do you want that?"
+          want_banana = gets.chomp.downcase
+          if want_banana == "yes"
+            @inventory[freebie.name.to_sym][:quantity] = @inventory[freebie.name.to_sym][:quantity] * groups_of_four
+            return @inventory[freebie.name.to_sym][:quantity]
+          else
+            puts "Ok, here's your receipt."
+          end
+      end
+    end
   end
   def show
     @inventory.each do |key, value|
@@ -48,35 +87,43 @@ end
 # :watermelon 50$
 
 apple = Item.new("apple", 10)
-
-banana = Item.new("banana", 20)
 oranges = Item.new("oranges", 5)
-watermelon = Item.new("watermelon", 50)
 grapes = Item.new("grapes", 15)
+banana = Item.new("banana", 20)
+watermelon = Item.new("watermelon", 50)
 
-
-# product1[:apple] = 10
-# product2[:banana] = 20
-# product1 = Item.new("apple", 10)
-# product2 = Item.new("banana", 20)
 
 cart = ShoppingCart.new
 
-
+cart.add_item_to_cart (apple)
+cart.add_item_to_cart (apple)
+cart.add_item_to_cart (apple)
+cart.add_item_to_cart (apple)
+cart.add_item_to_cart (apple)
+cart.add_item_to_cart (apple)
+cart.add_item_to_cart (apple)
 cart.add_item_to_cart (apple)
 cart.add_item_to_cart (banana)
 cart.add_item_to_cart (banana)
 cart.add_item_to_cart (oranges)
 cart.add_item_to_cart (watermelon)
 cart.add_item_to_cart (grapes)
+cart.add_item_to_cart (grapes)
+cart.add_item_to_cart (grapes)
+cart.add_item_to_cart (grapes)
+cart.add_item_to_cart (grapes)
+cart.add_item_to_cart (grapes)
+cart.add_item_to_cart (grapes)
+cart.add_item_to_cart (grapes)
 
 
-# cart.add_item_to_cart :apple
-# cart.add_item_to_cart :banana
-# cart.add_item_to_cart :banana
 
+cart.two_for_one(banana)
+cart.three_for_two(apple)
+cart.get_free_banana(grapes, banana)
 cart.show
-# 1 apple: 10$
-# 2 bananas: 40$
 cart.cost
-# cart.cost = 50
+# #
+# banana_deal = Deals.new(cart)
+#
+# banana_deal.two_for_one(banana)
