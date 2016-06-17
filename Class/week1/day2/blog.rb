2229954520d17ebb7517565e2e55d84a5a79dc83
alina <aliina.tudose@gmail.com>
2016-06-17 16:24:31 +0200

    add pagination to blog

--- Class/week1/day2/blog.rb
+++ Class/week1/day2/blog.rb
@@ -1,5 +1,6 @@
 
 require 'date'
+require 'colorize'
 require 'pry'
 
 class Blog
@@ -11,97 +12,47 @@ class Blog
   def add_post(post)
     @posts.push(post)
   end
-  def create_pages
-    sorted_posts = @posts.sort! do |post1, post2|
-      post2.date <=> post1.date
-    end
-    counter = 0
-    while counter <= 3 do
-      @posts.each do |post|
-        @page.push(post)
-        #binding.pry
-      end
-      counter += 1
-    end
-    if counter == 4
-      puts "prev < > next"
-      puts "Where do you want to go next? (Type '<' or '>' to navigate through the posts)"
-      choice = gets.chomp.downcase
-      if choice === "<"
-        while counter <= 4 do
-          @page.push(post)
-          puts post.title
-          #binding.pry
-          counter += 1
-        end
-      elsif choice === ">"
-        while counter <= 4 do
-          @page.push(post)
-          puts post.title
-          #binding.pry
-          counter += 1
-        end
-      else
-        puts "Command is unknown. Please try 'prev' or 'next'"
-      end
-    end
-    @all_pages.push(@page)
-  end
   def publish_front_page
     sorted_posts = @posts.sort! do |post1, post2|
       post2.date <=> post1.date
     end
     puts "---\nWelcome! This is the front page of my blog.\n---"
-    sorted_posts.each do |post|
-      if post.sponsored
-        puts "***SPONSORED POST***"
-        puts "#{post.title.upcase}"
-        puts "#{post.date}"
-        puts "-------------"
-        puts "#{post.text}"
-        puts "********************"
-        puts " "
+    split_posts = @posts.each_slice(3).to_a
+    split_posts.map.with_index do |page, index|
+      # binding.pry
+      page.each do |post|
+        if post.sponsored
+          puts "***SPONSORED POST***".colorize(:blue)
+          puts "#{post.title.upcase}"
+          puts "#{post.date}"
+          puts "-------------".colorize(:light_blue)
+          puts "#{post.text}"
+          puts "********************"
+          puts " "
+        else
+          puts "#{post.title.upcase}"
+          puts "#{post.date}"
+          puts "-------------".colorize(:light_blue)
+          puts "#{post.text}"
+          puts " "
+        end
+      end
+
+      puts "< prev next >".colorize(:color => :white, :background => :black)
+      command = gets.chomp.downcase
+      if command == ">" || command == "next"
+        paginated = split_posts.each
+        puts paginated.next
+      elsif command == "<" || command == "prev"
+        if index > 0
+          puts split_posts[index-1]
+        end
       else
-        puts "#{post.title.upcase}"
-        puts "#{post.date}"
-        puts "-------------"
-        puts "#{post.text}"
-        puts " "
+        puts "Sorry, I don't understand your command."
       end
     end
   end
 end
-
-# class BlogPage < Blog
-#   def add_post(post)
-#     length = @posts.length
-#     while length < 5 do
-#       @posts.push(post)
-#     end
-#   end
-#   def publish_regular_page
-#     page_sorted_posts = @posts.sort! do |post1, post2|
-#       post2.date <=> post1.date
-#     end
-#     page_sorted_posts.each do |post|
-#       if post.sponsored
-#         puts "***SPONSORED POST***"
-#         puts "#{post.title.upcase}"
-#         puts "#{post.date}"
-#         puts "-------------"
-#         puts "#{post.text}"
-#         puts "********************"
-#         puts " "
-#       else
-#         puts "#{post.title.upcase}"
-#         puts "#{post.date}"
-#         puts "-------------"
-#         puts "#{post.text}"
-#         puts " "
-#       end
-#     end
-#   end
-# end
 
 class Post
   attr_reader :title, :date, :text, :sponsored
@@ -119,17 +70,20 @@ yesterday = Date.new(2016,6,13)
 today = Date.new(2016,6,14)
 tomorrow = Date.new(2016,6,15)
 
+content1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+content2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+content3 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+content4 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+content5 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+content6 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+content7 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
 
-blog.add_post Post.new("Alina's post", yesterday, "content1" )
-blog.add_post Post.new("Marjon's post", tomorrow, "content2", true )
-blog.add_post Post.new("Ben's post", today, "content3" )
-blog.add_post Post.new("Mary's post", yesterday, "content1" )
-blog.add_post Post.new("Luke's post", tomorrow, "content2", true )
-blog.add_post Post.new("Chris's post", today, "content3" )
-blog.add_post Post.new("Elise's post", yesterday, "content1" )
-blog.add_post Post.new("John's post", today, "content3" )
-blog.add_post Post.new("Bobs post", yesterday, "content1" )
-blog.add_post Post.new("Carmen's post", today, "content3" )
-blog.add_post Post.new("Joana's post", yesterday, "content1" )
+blog.add_post Post.new("Alina's post", yesterday, content1 )
+blog.add_post Post.new("Marjon's post", tomorrow, content2, true )
+blog.add_post Post.new("Ben's post", today, content3 )
+blog.add_post Post.new("Mary's post", yesterday, content4)
+blog.add_post Post.new("Luke's post", tomorrow, content5, true )
+blog.add_post Post.new("Chris's post", today, content6 )
+blog.add_post Post.new("Elise's post", yesterday, content7 )
 
-blog.create_pages
+blog.publish_front_page
