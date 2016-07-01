a6ec9536111610abc94ba68767fa01c9ba8c8604
alina <aliina.tudose@gmail.com>
2016-07-01 19:18:29 +0200

    vikings fight saxons

--- Class/week3/day4/vikings/main.js
+++ Class/week3/day4/vikings/main.js
@@ -1,4 +1,5 @@
-var pry = require('pryjs')
+var pry = require('pryjs');
+var colors = require('colors');
 
 function Viking(name, health, strength){
   this.name = name;
@@ -6,11 +7,11 @@ function Viking(name, health, strength){
   this.strength = strength;
 }
 
-var Jon = new Viking('Jon', 100, 100);
-var Boo = new Viking('Boo', 100, 90);
-var Kadi = new Viking('Kadi', 100, 80);
-var Mulu = new Viking('Mulu', 100, 70);
-var Baab = new Viking('Baab', 100, 60);
+var Jon = new Viking('Jon', 1000, 20);
+var Boo = new Viking('Boo', 1000, 22);
+var Kadi = new Viking('Kadi', 1000, 10);
+var Mulu = new Viking('Mulu', 1000, 15);
+var Baab = new Viking('Baab', 1000, 12);
 
 vikings = []
 
@@ -20,17 +21,19 @@ vikings.push(Kadi);
 vikings.push(Mulu);
 vikings.push(Baab);
 
+
 function Saxon(health, strength){
+  this.name = 'Saxon';
   this.health = health;
   this.strength = strength;
-  this.name = 'Saxon';
 }
 
-var sax1 = new Saxon(100, 4)
-var sax2 = new Saxon(100, 7)
-var sax3 = new Saxon(100, 3)
-var sax4 = new Saxon(100, 2)
-var sax5 = new Saxon(100, 1)
+var sax1 = new Saxon(1000, 4)
+var sax2 = new Saxon(1000, 7)
+var sax3 = new Saxon(1000, 3)
+var sax4 = new Saxon(1000, 2)
+var sax5 = new Saxon(1000, 1)
+
 
 saxons = []
 
@@ -51,8 +54,10 @@ function Attacks(type, damage){
   this.type = type
   this.damage = damage
 }
-var punch = new  Attacks(' 👊 ', .4);
-var kick = new Attacks(' 💪 ', .2  );
+
+var punch = new  Attacks(' 👊 ', 20);
+var kick = new Attacks(' 💪 ', 100  );
+
 var attacks = [];
 attacks.push(punch);
 attacks.push(kick);
@@ -72,42 +77,50 @@ function selectDamage(type){
   }
   console.log(theHits);
   return damage
-  // eval(pry.it);
 }
 
+
 function fight(vikings, saxons, attacks){
+
   var fighter1 = chooseFighter(vikings);
   var fighter2 = chooseFighter(saxons);
   var type1 = chooseAttack(attacks);
   var type2 = chooseAttack(attacks);
-  // console.log(fighter1.name + ' and ' + fighter2.name + ' are fighting!')
-  //
-  // eval(pry.it);
 
-  fighter2.health -= selectDamage(type1) * fighter2.strength;
-  fighter1.health -= selectDamage(type2) * fighter1.strength;
+  fighter1.health -= selectDamage(type1) * fighter1.strength;
+  fighter2.health -= selectDamage(type2) * fighter2.strength;
 
+  console.log(fighter1.name+'\'s health is '+ fighter1.health );
+  console.log(fighter2.name+'\'s health is '+ fighter2.health );
 
-  console.log(fighter1.name+'\'s health is '+fighter1.health );
-  console.log(fighter2.name+'\'s health is '+fighter2.health );
+  if(vikings.length > 1 && saxons.length > 1){
+    if (fighter1.health > 0 && fighter2.health > 0){
+    } else if(fighter1.health > fighter2.health){
+      var winner = fighter1.name
+      var dead = fighter2.name
+      var index = saxons.indexOf(fighter2);
+      console.log('☠☠☠ ' + dead + ' is DEAD ☠☠☠'.bold.red)
+      saxons.splice(index, 1);
+      saxons.length --
+      // eval(pry.it)
+    } else{
+      var winner = fighter2.name
+      var dead = fighter1.name
+      var index = vikings.indexOf(fighter1);
+      console.log('☠☠☠ ' + dead + ' is DEAD ☠☠☠'.bold.red)
+      vikings.splice(index, 1);
+      vikings.length --
+    }
 
-  if (fighter1.health > 0 && fighter2.health > 0){
-    fight(vikings, saxons, attacks);
-  } else if(fighter1.health > fighter2.health){
-    var winner = fighter1.name
-    var dead = fighter2.name
-    console.log(dead + ' is dead!')
+    // eval(pry.it)
+    setTimeout(function(){
+      fight(vikings, saxons, attacks)
+    }, 1000);
+  } else if(vikings.length > saxons.length){
+    console.log('\n▲▲▲▲▲ VIKINGS WIN ▲▲▲▲▲\n'.rainbow);
   } else{
-    var winner = fighter2.name
-    var dead = fighter1.name
-    console.log(dead + ' is dead!')
+    console.log('\n▲▲▲▲▲ SAXONS WIN ▲▲▲▲▲\n'.rainbow);
   }
-
-  return winner
 }
 
-
 fight(vikings, saxons, attacks);
-
-// console.log(chooseFighter(vikings))
-// console.log(selectDamage(attacks, times))
